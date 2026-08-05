1b8e9dd0513a7830fe69cd6c135c5ab7416fc926
Karankhatr1 <160300429+Karankhatr1@users.noreply.github.com>
2026-08-05 20:53:51 +0530

    Update data.js

--- design/data.js
+++ design/data.js
@@ -25,7 +25,7 @@ const VIEW = {
 
   availability: "Open to lead roles",  // Little green status chip
 
-  avatar: "images/avatar.svg",         // Photo for THIS view → put it in design/images/
+  avatar: "images/Karan.png",         // Photo for THIS view → put it in design/images/
   resume: "",                          // Link to your design resume PDF, or leave ""
 
   about:
